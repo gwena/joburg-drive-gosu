ec9a819fee406e4d2bff7612e4e254238c3a5ff3
Gwenael Ropert <gwena.code@gmail.com>
2017-05-01 17:01:26 +0200

    Add sound for minibus

--- lib/minibus_taxi.rb
+++ lib/minibus_taxi.rb
@@ -7,7 +7,8 @@ class MinibusTaxi < Vehicule
   OFFSET_PER_DRIFT = 0.25
 
   def initialize(window)
-    super(window, 500, 450, 0.5, 0, "media/Cars/MinibusTaxi.png")
+    super(window, 500, 450, 0.5, 0, 'media/Cars/MinibusTaxi.png')
+    @stop_sound = Gosu::Sample.new(window, 'media/sound/carscreech.wav')
     @current_action = :stop
     @nb_drift = 0
     @offset_dir = OFFSET_PER_DRIFT
@@ -17,6 +18,11 @@ class MinibusTaxi < Vehicule
     @current_action = ACTIONS[rand(ACTIONS.size)] if rand(NB_FRAME_PROBALITY_TO_CHANGE) == 0
     send(@current_action)
     drift
+    super
+  end
+
+  def stop
+    @stop_sound.play
     super
   end
 
