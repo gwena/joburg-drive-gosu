ed82a412b2c6dd28af1bbbb0aafdc7683c1c8afe
Gwenael Ropert <gwena.code@gmail.com>
2017-05-14 12:14:55 +0200

    Feature - Increase Viper speed by 3

--- lib/viper.rb
+++ lib/viper.rb
@@ -3,7 +3,7 @@ require_relative 'autonomous_vehicle'
 # Fast autonomous car
 class Viper < AutonomousVehicle
   def initialize(window)
-    super(window, 400, 450, 1, 0, 'media/img/cars/Black_viper.png')
+    super(window, 400, 450, 3, 0, 'media/img/cars/Black_viper.png')
     @dir = :stop
   end
 end
