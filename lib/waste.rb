deca377f07bbab79a01f628c2ae6e0063d0d6299
Gwenael Ropert <gwena.code@gmail.com>
2017-05-22 17:24:13 +0200

    Feature - Create Waste superclass

---
+++ lib/waste.rb
@@ -0,0 +1,21 @@
+require 'gosu'
+
+# Superclass for all the Waste types
+class Waste
+  attr_reader :x, :y
+
+  def initialize(window, x, y)
+    @window = window
+    @x = x
+    @y = y
+    @image = Gosu::Image.new("media/img/waste/#{image}")
+  end
+
+  def update
+    raise NotImplementedError, "update() needs to be implemented in the Waste subclasses"
+  end
+
+  def draw
+    @image.draw_rot(@x - @window.x, @y - @window.y, 1, 0)
+  end
+end
